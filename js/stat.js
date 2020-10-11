56aa7e7b35238d2794d00e7e70d0ea0bf5c3b9d8
Andrey Demyanov <demyanov23@gmail.com>
2020-10-11 12:46:44 +0300

    Вносит правки после проверки линтером

--- js/stat.js
+++ js/stat.js
@@ -12,14 +12,14 @@ var BAR_WIDTH = 40;
 var tableX = CLOUD_X + BAR_WIDTH;
 var tableY = CLOUD_HEIGHT - GAP * 3;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
-  
+
   for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
@@ -29,49 +29,49 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(
-    ctx,
-    CLOUD_X + GAP,
-    CLOUD_Y + GAP,
-    'rgba(0, 0, 0, 0.7)'
+      ctx,
+      CLOUD_X + GAP,
+      CLOUD_Y + GAP,
+      'rgba(0, 0, 0, 0.7)'
   );
   renderCloud(
-    ctx,
-    CLOUD_X,
-    CLOUD_Y,
-    '#fff'
+      ctx,
+      CLOUD_X,
+      CLOUD_Y,
+      '#fff'
   );
-  
+
   ctx.fillStyle = '#000';
-  
+
   var maxTime = getMaxElement(times);
-  
+
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP + GAP, CLOUD_Y + GAP + GAP);
   ctx.fillText('Список результатов:', CLOUD_X + GAP + GAP, 48);
-  
+
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillText(
-      names[i],
-      tableX + (BAR_WIDTH + TEXT_WIDTH) * i,
-      tableY
+        names[i],
+        tableX + (BAR_WIDTH + TEXT_WIDTH) * i,
+        tableY
     );
 
     if (names[i] === 'Вы') {
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
     else {
-        ctx.fillStyle = 'hsl(240,' + Math.random() * 100 + '%, 50%)';
+      ctx.fillStyle = 'hsl(240,' + Math.random() * 100 + '%, 50%)';
     };
 
     ctx.fillRect(
-      tableX + (BAR_WIDTH + TEXT_WIDTH) * i,
-      tableY - GAP,
-      BAR_WIDTH,
-      -(((BAR_HEIGHT * times[i]) / maxTime) - FONT_GAP - GAP)
+        tableX + (BAR_WIDTH + TEXT_WIDTH) * i,
+        tableY - GAP,
+        BAR_WIDTH,
+        -(((BAR_HEIGHT * times[i]) / maxTime) - FONT_GAP - GAP)
     );
 
     ctx.fillStyle = 'black';
